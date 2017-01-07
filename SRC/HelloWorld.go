b38b36dd43241b600203c4c0ad19cafec19077e0
Nonthawat Duangchai <nonthawat2538@hotmail.com>
2017-01-07 13:43:31 +0700

    Go Learning 001 - Create Project

---
+++ SRC/HelloWorld.go
@@ -0,0 +1,10 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, world, This is my first google go program :)")
+	fmt.Println("Hello this is from a IDE for creating go programs!")
+}
